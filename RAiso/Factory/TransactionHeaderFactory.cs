5837272938a80d961da875b63aecd0b82bff6d15
rxyhn <rxyhn.dev@gmail.com>
2024-03-16 21:06:00 +0700

    feat: add Factory

---
+++ RAiso/Factory/TransactionHeaderFactory.cs
@@ -0,0 +1,20 @@
+﻿using RAiso.Model;
+using System;
+
+namespace RAiso.Factory
+{
+    public class TransactionHeaderFactory
+    {
+        public static TransactionHeader CreateTransactionHeader(int tId, int uId, DateTime date)
+        {
+            TransactionHeader th = new TransactionHeader
+            {
+                TransactionID = tId,
+                UserID = uId,
+                TransactionDate = date,
+            };
+
+            return th;
+        }
+    }
+}
