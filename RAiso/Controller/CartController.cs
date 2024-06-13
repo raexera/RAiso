5be33ffdc701e7ce6f90900f13d11f20e5f5356a
rxyhn <rxyhn.dev@gmail.com>
2024-06-13 18:05:06 +0700

    feat: add Controller

---
+++ RAiso/Controller/CartController.cs
@@ -0,0 +1,39 @@
+﻿using RAiso.Handler;
+using RAiso.Model;
+using System.Collections.Generic;
+
+namespace RAiso.Controller
+{
+    public class CartController
+    {
+        public static List<Cart> GetCarts(int uId)
+        {
+            return CartHandler.GetCarts(uId);
+        }
+
+        public static void AddToCart(int uId, int sId, int qty)
+        {
+            CartHandler.HandleAddToCart(uId, sId, qty);
+        }
+
+        public static Cart GetCart(int uId, int sId)
+        {
+            return CartHandler.GetCart(uId, sId);
+        }
+
+        public static void UpdateCart(Cart cart, int qty)
+        {
+            CartHandler.HandleUpdate(cart, qty);
+        }
+
+        public static void RemoveCart(Cart cart)
+        {
+            CartHandler.HandleDelete(cart);
+        }
+
+        public static void Checkout(List<Cart> carts)
+        {
+            TransactionHandler.Checkout(carts);
+        }
+    }
+}
---
+++ RAiso/Controller/UserController.cs
@@ -0,0 +1,151 @@
+﻿using RAiso.Handler;
+using RAiso.Model;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RAiso.Controller
+{
+    public class UserController
+    {
+        private static string ValidateField(string fieldValue, Func<string, string> validationFunction)
+        {
+            string result = validationFunction(fieldValue);
+            return result;
+        }
+
+        public static string CheckUsername(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return "Username must be filled";
+            return "";
+        }
+
+        public static string CheckPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "Password must be filled";
+            return "";
+        }
+
+        public static string ValidateLogin(string name, string password)
+        {
+            string result = CheckUsername(name);
+            if (string.IsNullOrEmpty(result))
+                result = CheckPassword(password);
+            if (string.IsNullOrEmpty(result))
+                result = UserHandler.HandleLogin(name, password);
+            return result;
+        }
+
+        public static string CheckUsernameRegister(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return "Username must be filled";
+            if (name.Length < 5 || name.Length > 50)
+                return "Username must be between 5 and 50 characters";
+            return "";
+        }
+
+        public static string CheckDOB(string dob)
+        {
+            if (string.IsNullOrEmpty(dob))
+                return "Date of Birth must be filled";
+
+            if ((DateTime.Now - DateTime.Parse(dob)).TotalDays < 365)
+                return "Must be at least 1 year of age";
+
+            return "";
+        }
+
+        public static string CheckGender(string gender)
+        {
+            if (string.IsNullOrEmpty(gender))
+                return "Gender must be selected";
+            return "";
+        }
+
+        public static string CheckAddress(string address)
+        {
+            if (string.IsNullOrEmpty(address))
+                return "Address must be filled";
+            return "";
+        }
+
+        public static string CheckPasswordRegister(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "Password must be filled";
+            if (!Regex.IsMatch(password, "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$"))
+                return "Password must be alphanumeric";
+            return "";
+        }
+
+        public static string CheckPhone(string phone)
+        {
+            if (string.IsNullOrEmpty(phone))
+                return "Phone must be filled";
+            return "";
+        }
+
+        public static string ValidateRegister(string name, string dob, string gender, string address, string password, string phone)
+        {
+            string result = ValidateField(name, CheckUsernameRegister);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(dob, CheckDOB);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(gender, CheckGender);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(address, CheckAddress);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(password, CheckPasswordRegister);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(phone, CheckPhone);
+            if (string.IsNullOrEmpty(result))
+                result = UserHandler.HandleRegister(name);
+            return result;
+        }
+
+        public static string ValidateUpdate(string name, string dob, string gender, string address, string password, string phone, string nameFirst)
+        {
+            string result = ValidateField(name, CheckUsernameRegister);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(dob, CheckDOB);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(gender, CheckGender);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(address, CheckAddress);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(password, CheckPasswordRegister);
+            if (string.IsNullOrEmpty(result))
+                result = ValidateField(phone, CheckPhone);
+            if (string.IsNullOrEmpty(result))
+                result = UserHandler.HandleUpdate(name, nameFirst);
+            return result;
+        }
+
+        public static string GetRole(string id)
+        {
+            return UserHandler.GetRole(id);
+        }
+
+        public static MsUser GetUserByName(string name)
+        {
+            return UserHandler.GetUserByName(name);
+        }
+
+        public static MsUser GetUserById(string id)
+        {
+            return UserHandler.GetUserById(id);
+        }
+
+        public static void InsertUser(string name, DateTime dob, string gender, string address, string password, string phone)
+        {
+            UserHandler.HandleInsert(name, dob, gender, address, password, phone);
+        }
+
+        public static void UpdateUser(string name, DateTime dob, string gender, string address, string password, string phone, int id)
+        {
+            UserHandler.UpdateUser(name, dob, gender, address, password, phone, id);
+        }
+    }
+}
